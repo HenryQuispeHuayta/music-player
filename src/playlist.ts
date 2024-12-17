f9d4084890e8910694c26d200fe7256b18cabf47
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2024-12-16 23:16:10 -0400

    add playlist

---
+++ src/playlist.ts
@@ -0,0 +1,16 @@
+export const playlist = [
+  {
+    id: 1,
+    title: "Lost in the City Lights",
+    artist: "Cosmo Sheldrake",
+    cover: "/cover/cover-1.png",
+    song: "/music/lost-in-city-lights-145038.mp3",
+  },
+  {
+    id: 2,
+    title: "Forest Lullaby",
+    artist: "Lesfm",
+    cover: "/cover/cover-2.png",
+    song: "/music/forest-lullaby-110624.mp3",
+  },
+];
